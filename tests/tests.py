d879cc1c4d233a4190e61a0fa7a33160c8b3a438
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-08-21 12:46:51 +0200

    fix test case

--- tests/tests.py
+++ tests/tests.py
@@ -16,7 +16,7 @@ def test1():
 def test2():
     from pyXLMS import parser
     crosslink = parser.create_crosslink("PEPTIDE", 2, "PEPTIDE", 1)
-    assert crosslink["alpha_peptide"] == "EDITPEP"
+    assert crosslink["alpha_peptide"] == "PEPTIDE"
     assert crosslink["alpha_crosslink_position"] == 1
     assert crosslink["beta_peptide"] == "PEPTIDE"
     assert crosslink["beta_crosslink_position"] == 2
