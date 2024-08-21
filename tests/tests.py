33ae3e9a7af6e3a4839a5862b43dfeab48b9cf7f
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-08-21 12:44:26 +0200

    implement create_crosslink

--- tests/tests.py
+++ tests/tests.py
@@ -6,4 +6,17 @@
 # micha.birklbauer@gmail.com
 
 def test1():
-    assert True
+    from pyXLMS import parser
+    crosslink = parser.create_crosslink("PEPTIDE", 1, "EDITPEP", 2)
+    assert crosslink["alpha_peptide"] == "EDITPEP"
+    assert crosslink["alpha_crosslink_position"] == 2
+    assert crosslink["beta_peptide"] == "PEPTIDE"
+    assert crosslink["beta_crosslink_position"] == 1
+
+def test2():
+    from pyXLMS import parser
+    crosslink = parser.create_crosslink("PEPTIDE", 2, "PEPTIDE", 1)
+    assert crosslink["alpha_peptide"] == "EDITPEP"
+    assert crosslink["alpha_crosslink_position"] == 1
+    assert crosslink["beta_peptide"] == "PEPTIDE"
+    assert crosslink["beta_crosslink_position"] == 2
