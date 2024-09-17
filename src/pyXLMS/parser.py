4dcf32d262a8da9cb52d2a7fca9d48df5f0d2185
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-09-17 15:46:23 +0200

    Create parser.py

---
+++ src/pyXLMS/parser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# 2024 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+import pandas as pd
+from .data import create_crosslink
+from .data import create_csm
+from .transform import to_dataframe
+
+from typing import BinaryIO
+from typing import Union
+from typing import Any
+
+def read_custom():
+    return
+
+
+def read_msannika(input: Union[str, BinaryIO], format: str = "auto", sep: str = "\t"):
+    if format == "auto" and type(input) is not str:
+        raise ValueError("Can't detect format for file-like objects. Please specify format manually!")
+
+    return
+
+
+def read():
+    return
