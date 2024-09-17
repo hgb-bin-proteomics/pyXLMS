c488d9c2d77cb918cdad3b927e5efddc3040464f
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-09-17 15:54:12 +0200

    extend parser

--- src/pyXLMS/parser.py
+++ src/pyXLMS/parser.py
@@ -5,6 +5,8 @@
 # micha.birklbauer@gmail.com
 
 import pandas as pd
+from os.path import splitext
+
 from .data import create_crosslink
 from .data import create_csm
 from .transform import to_dataframe
@@ -18,9 +20,17 @@ def read_custom():
 
 
 def read_msannika(input: Union[str, BinaryIO], format: str = "auto", sep: str = "\t"):
+    data = None
     if format == "auto" and type(input) is not str:
         raise ValueError("Can't detect format for file-like objects. Please specify format manually!")
-
+    if format == "auto":
+        file_extension = splitext(input)
+        if file_extension == ".tsv" or file_extension == ".csv":
+            data = pd.read_csv(input, sep = sep)
+        elif file_extension == ".xlsx":
+            data = pd.read_excel(input, engine = "openpyxl")
+        else:
+            raise ValueError("")
     return
 
 
