eae2e03fae0659fc73da6b6b6dbe86f24a8f11e2
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-09-18 14:13:25 +0200

    add parser result

--- src/pyXLMS/parser.py
+++ src/pyXLMS/parser.py
@@ -12,8 +12,36 @@ from .data import create_csm
 from .transform import to_dataframe
 
 from typing import BinaryIO
+from typing import List
+from typing import Dict
 from typing import Union
 from typing import Any
+
+def create_parser_result(search_engine: str, csms: List[Dict[str, any]], crosslinks: List[Dict[str, Any]]) -> Dict[str, Any]:
+    """Creates a parser result data structure.
+
+    Contains all necessary data elements that should be contained in a result returned by a crosslink search engine result parser.
+
+    Parameters
+    ----------
+    search_engine : str
+        Name of the identifying crosslink search engine.
+    csms : list of dict
+        List of crosslink-spectrum-matches as created by `data.create_csm`.
+    crosslinks : list of dict
+        List of crosslinks as created by `data.create_crosslink`.
+
+    Returns
+    -------
+    dict
+        The parser result data structure which is a dictionary with keys "data_type", "search_engine", "crosslink-spectrum-matches" and
+        "crosslinks".
+    """
+    return {"data_type": "parser_result",
+            "search_engine": search_engine,
+            "crosslink-spectrum-matches": csms,
+            "crosslinks": crosslinks}
+
 
 def read_custom():
     return
