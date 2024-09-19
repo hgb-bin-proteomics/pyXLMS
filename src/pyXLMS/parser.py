8be18effadd0e3cbb71f36c3fea38ffc1fd328ac
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-09-19 14:54:23 +0200

    update ms annika parser

--- src/pyXLMS/parser.py
+++ src/pyXLMS/parser.py
@@ -11,7 +11,6 @@ from os.path import splitext
 
 from .data import create_crosslink
 from .data import create_csm
-from .transform import to_dataframe
 
 from typing import BinaryIO
 from typing import List
@@ -19,6 +18,7 @@ from typing import Dict
 from typing import Literal
 from typing import Any
 
+# todo: move to data
 def create_parser_result(search_engine: str, csms: List[Dict[str, any]] | None, crosslinks: List[Dict[str, Any]] | None) -> Dict[str, Any]:
     """Creates a parser result data structure.
 
@@ -71,6 +71,7 @@ def read_msannika(input: str | BinaryIO, format: Literal["auto", "csv", "tsv", "
     format : "auto", "csv", "tsv", or "xlsx", default = "auto"
         The format of the result file. ``"auto"`` is only available if the name/path to the MS Annika result file is given.
     """
+    ## reading data
     data = None
     if format == "auto" and type(input) is not str:
         raise ValueError("Can't detect format for file-like objects. Please specify format manually!")
@@ -81,12 +82,70 @@ def read_msannika(input: str | BinaryIO, format: Literal["auto", "csv", "tsv", "
         elif file_extension == ".xlsx":
             data = pd.read_excel(input, engine = "openpyxl")
         else:
-            raise ValueError("")
+            raise ValueError(f"Detected file extension {file_extension} is not supported! Input file has to be a valid file with extension '.csv', '.tsv' or '.xlsx'!")
     elif format in ["csv", "tsv", "xlsx"]:
-        pass
+        if format == "xlsx":
+            data = pd.read_excel(input, engine = "openpyxl")
+        else:
+            data = pd.read_csv(input, sep = sep)
     else:
-        raise ValueError("")
-    return
+        raise ValueError(f"Provided input format {format} is not supported! Input format has to be of type 'csv', 'tsv' or 'xlsx'!")
+    if data is None:
+        raise RuntimeError("Something went wrong while reading the file! Please file a bug report!")
+    ## detect input file type
+    col_names = data.columns.values.tolist()
+    is_crosslink_dataframe = "# CSMs" in col_names
+    ## process data
+    crosslinks = list()
+    csms = list()
+    if is_crosslink_dataframe:
+        for i, row in data.iterrows():
+            # create crosslink
+            crosslink = create_crosslink(peptide_a = "",
+                                         xl_position_peptide_a = 0,
+                                         proteins_a = [""],
+                                         xl_position_proteins_a = [0],
+                                         decoy_a = False,
+                                         peptide_b = "",
+                                         xl_position_peptide_b = 0,
+                                         proteins_b = [""],
+                                         xl_position_proteins_b = [0],
+                                         decoy_b = False,
+                                         score = 0.0)
+            crosslinks.append(crosslink)
+    else:
+        for i, row in data.iterrows():
+            # create csm
+            csm = create_csm(peptide_a = "",
+                             modifications_a = {},
+                             xl_position_peptide_a = 0,
+                             proteins_a = [""],
+                             xl_position_proteins_a = [0],
+                             pep_position_proteins_a = [0],
+                             score_a = 0.0,
+                             decoy_a = False,
+                             peptide_b = "",
+                             modifications_b = {},
+                             xl_position_peptide_b = 0,
+                             proteins_b = [""],
+                             xl_position_proteins_b = [0],
+                             pep_position_proteins_b = [0],
+                             score_b = 0.0,
+                             decoy_b = False,
+                             score = 0.0,
+                             spectrum_file = "",
+                             scan_nr = 0,
+                             charge = 0,
+                             rt = 0.0,
+                             im_cv = None)
+            csms.append(csm)
+    ## check results
+    if len(crosslinks) + len(csms) == 0:
+        raise RuntimeError("No crosslink-spectrum-matches or crosslinks were parsed! If this is unexpected, please file a bug report!")
+    ## return parser result
+    return create_parser_result(search_engine = "MS Annika",
+                                csms = csms if len(csms) > 0 else None,
+                                crosslinks = crosslinks if len(crosslinks) > 0 else None)
 
 
 def read():
