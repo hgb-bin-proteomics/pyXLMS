c0dd5d372e408ad1c041592c6d6dc14d473c3432
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-11-26 14:41:30 +0100

    add modification dict

--- src/pyXLMS/constants.py
+++ src/pyXLMS/constants.py
@@ -5,3 +5,5 @@
 # micha.birklbauer@gmail.com
 
 AMINO_ACIDS = {"A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "U", "W", "Y", "V", "J", "O"}
+MODIFICATIONS = {"Carbamidomethyl": 57.021464,
+                 "Oxidation": 15.994915}
