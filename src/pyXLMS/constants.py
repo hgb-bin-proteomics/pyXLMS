d2c86bc758b8b7f7c0a3b920afc4b2048c690481
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-11-26 13:22:28 +0100

    add constants

---
+++ src/pyXLMS/constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# 2024 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+AMINO_ACIDS = {"A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "U", "W", "Y", "V", "J", "O"}
