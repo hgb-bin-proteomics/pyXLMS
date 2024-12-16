20156023af73477c2b6b1e1379374f224c4f9569
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-12-16 13:04:57 +0100

    add constants docs

--- src/pyXLMS/constants.py
+++ src/pyXLMS/constants.py
@@ -29,6 +29,19 @@ AMINO_ACIDS = {
     "J",
     "O",
 }
+"""List of valid amino acids.
+
+List of one-letter codes for all valid amino acids.
+
+Examples
+--------
+>>> from pyXLMS.constants import AMINO_ACIDS
+>>> "A" in AMINO_ACIDS
+True
+>>> "B" in AMINO_ACIDS
+False
+"""
+
 CROSSLINKERS = {
     "BS3": 138.06808,
     "DSS": 138.06808,
@@ -37,10 +50,36 @@ CROSSLINKERS = {
     "DSBSO": 308.03883,
     "PhoX": 209.97181,
 }
+"""Dictionary of crosslinkers.
+
+Dictionary of pre-defined crosslinkers that maps crosslinker names to crosslinker delta masses.
+Currently contains `"BS3"`, `"DSS"`, `"DSSO"`, `"ADH"`, `"DSBSO"`, `"PhoX"`.
+
+Examples
+--------
+>>> from pyXLMS.constants import CROSSLINKERS
+>>> CROSSLINKERS["BS3"]
+138.06808
+"""
+
 MODIFICATIONS = {
     "Carbamidomethyl": 57.021464,
     "Oxidation": 15.994915,
     "Phospho": 79.966331,
     "Acetyl": 42.010565,
 }
+"""Dictionary of post-translational-modifications.
+
+Dictionary of pre-defined post-translational-modifications that maps modification names to modification delta masses.
+Currently contains `"Carbamidomethyl"`, `"Oxidation"`, `"Phospho"`, `"Acetyl"` and all crosslinkers.
+
+Examples
+--------
+>>> from pyXLMS.constants import MODIFICATIONS
+>>> MODIFICATIONS["Carbamidomethyl"]
+57.021464
+>>> MODIFICATIONS["BS3"]
+138.06808
+"""
+
 MODIFICATIONS.update(CROSSLINKERS)
