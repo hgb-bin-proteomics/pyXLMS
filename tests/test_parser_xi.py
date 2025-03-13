6ef5db04741959b5764a2ea5742b55649497083e
Micha Birklbauer <micha.birklbauer@gmail.com>
2025-03-13 13:49:46 +0100

    add parser test 1

--- tests/test_parser_xi.py
+++ tests/test_parser_xi.py
@@ -39,3 +39,45 @@ def test2():
     assert pseq("KIECcmFDSVEMoxISGVEDR") == {4: "cm", 10: "ox"}
     assert pseq("KIECcmFDSVEISGVEDRMox") == {4: "cm", 17: "ox"}
     assert pseq("CcmKIECcmFDSVEISGVEDRMox") == {1: "cm", 5: "cm", 18: "ox"}
+
+
+def test3():
+    from pyXLMS import parser as p
+    from pyXLMS.transform import modifications_to_str as mts
+
+    pr = p.read_xi(XISEARCH)
+    assert pr["data_type"] == "parser_result"
+    assert pr["completeness"] == "partial"
+    assert pr["search_engine"] == "xiSearch/xiFDR"
+    assert pr["crosslink-spectrum-matches"] is not None
+    assert pr["crosslinks"] is None
+
+    csms = pr["crosslink-spectrum-matches"]
+    assert len(csms) == 4648
+
+    csm = csms[0]
+    assert csm["data_type"] == "crosslink-spectrum-match"
+    assert csm["completeness"] == "partial"
+    assert csm["alpha_peptide"] == "SDKNR"
+    assert mts(csm["alpha_modifications"]) == "(3:[BS3|138.06808])"
+    assert csm["alpha_peptide_crosslink_position"] == 3
+    assert csm["alpha_proteins"] == ["Cas9"]
+    assert csm["alpha_proteins_crosslink_positions"] == [866]
+    assert csm["alpha_proteins_peptide_positions"] == [864]
+    assert csm["alpha_score"] == pytest.approx(0.596154)
+    assert not csm["alpha_decoy"]
+    assert csm["beta_peptide"] == "SDKNR"
+    assert mts(csm["beta_modifications"]) == "(3:[BS3|138.06808])"
+    assert csm["beta_peptide_crosslink_position"] == 3
+    assert csm["beta_proteins"] == ["Cas9"]
+    assert csm["beta_proteins_crosslink_positions"] == [866]
+    assert csm["beta_proteins_peptide_positions"] == [864]
+    assert csm["beta_score"] == pytest.approx(0.596154)
+    assert not csm["beta_decoy"]
+    assert csm["crosslink-type"] == "intra"
+    assert csm["score"] == pytest.approx(8.758549)
+    assert csm["spectrum_file"] == "XLpeplib_Beveridge_QEx-HFX_DSS_R1.mgf"
+    assert csm["scan_nr"] == 2561
+    assert csm["charge"] == 3
+    assert csm["retention_time"] is None
+    assert csm["ion_mobility"] is None
