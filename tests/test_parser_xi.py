ed5188d3a5835e92370da07135b3177f0bdb42d8
Micha Birklbauer <micha.birklbauer@gmail.com>
2025-03-13 11:35:36 +0100

    fix import

--- tests/test_parser_xi.py
+++ tests/test_parser_xi.py
@@ -13,7 +13,7 @@ XIFDR_LINKS = "data/xi/1perc_xl_boost_Links_xiFDR2.2.1.csv"
 
 
 def test1():
-    from pyXLMS.parser_xi import detect_xi_file
+    from pyXLMS.parser_xi import detect_xi_filetype
     import pandas as pd
 
     xi = pd.read_csv(XISEARCH)
@@ -21,15 +21,15 @@ def test1():
     xls = pd.read_csv(XIFDR_LINKS)
     err = pd.read_excel("data/msannika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx")
 
-    assert detect_xi_file(xi) == "xisearch"
-    assert detect_xi_file(csms) == "xifdr_csms"
-    assert detect_xi_file(xls) == "xifdr_crosslinks"
+    assert detect_xi_filetype(xi) == "xisearch"
+    assert detect_xi_filetype(csms) == "xifdr_csms"
+    assert detect_xi_filetype(xls) == "xifdr_crosslinks"
 
     with pytest.raises(
         ValueError,
         match="Could not infer data source, are you sure you read a xi result file?",
     ):
-        _r = detect_xi_file(err)
+        _r = detect_xi_filetype(err)
 
 
 def test2():
