29764cabf502d9a48ab17c479e317d3ceca7bde8
Micha Birklbauer <micha.birklbauer@gmail.com>
2025-03-18 15:58:35 +0100

    update xifdr csms test

--- tests/test_parser_xi.py
+++ tests/test_parser_xi.py
@@ -203,3 +203,36 @@ def test5():
 
     csms = pr["crosslink-spectrum-matches"]
     assert len(csms) == 413
+
+    csm = csms[0]
+    assert csm["data_type"] == "crosslink-spectrum-match"
+    assert csm["completeness"] == "partial"
+    assert csm["alpha_peptide"] == "KIECFDSVEISGVEDR"
+    assert (
+        mts(csm["alpha_modifications"])
+        == "(1:[BS3|138.06807]);(4:[Carbamidomethyl|57.021464])"
+    )
+    assert csm["alpha_peptide_crosslink_position"] == 1
+    assert csm["alpha_proteins"] == ["Cas9"]
+    assert csm["alpha_proteins_crosslink_positions"] == [575]
+    assert csm["alpha_proteins_peptide_positions"] == [575]
+    assert csm["alpha_score"] is None
+    assert not csm["alpha_decoy"]
+    assert csm["beta_peptide"] == "KIECFDSVEISGVEDR"
+    assert (
+        mts(csm["beta_modifications"])
+        == "(1:[BS3|138.06807]);(4:[Carbamidomethyl|57.021464])"
+    )
+    assert csm["beta_peptide_crosslink_position"] == 1
+    assert csm["beta_proteins"] == ["Cas9"]
+    assert csm["beta_proteins_crosslink_positions"] == [575]
+    assert csm["beta_proteins_peptide_positions"] == [575]
+    assert csm["beta_score"] is None
+    assert not csm["beta_decoy"]
+    assert csm["crosslink-type"] == "intra"
+    assert csm["score"] == pytest.approx(27.268)
+    assert csm["spectrum_file"] == "XLpeplib_Beveridge_QEx-HFX_DSS_R1.mgf"
+    assert csm["scan_nr"] == 19140
+    assert csm["charge"] == 4
+    assert csm["retention_time"] is None
+    assert csm["ion_mobility"] is None
