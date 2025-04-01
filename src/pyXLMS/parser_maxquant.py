6d88ee44d5c6beb5a6f958d51a888e1cd9647a75
Micha Birklbauer <micha.birklbauer@gmail.com>
2025-04-01 16:24:46 +0200

    mq parser init

---
+++ src/pyXLMS/parser_maxquant.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+from __future__ import annotations
+
+import pandas as pd
+from tqdm import tqdm
