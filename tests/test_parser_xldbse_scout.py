a086d740bb07236e26b4cc7a5b60f9d57f115c2a
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-05-08 13:24:34 +0200

    add detect scout file type test

--- tests/test_parser_xldbse_scout.py
+++ tests/test_parser_xldbse_scout.py
@@ -14,3 +14,23 @@ SCOUT_CSMS_10 = "data/scout/Cas10_Unfiltered_CSMs.csv"
 SCOUT_CSMS_F_10 = "data/scout/Cas10_Filtered_CSMs.csv"
 SCOUT_XL_10 = "data/scout/Cas10_Residue_Pairs.csv"
 SCOUT_MM = "data/scout/Multi_Mod.csv"
+
+
+def test1():
+    from pyXLMS.parser import detect_scout_filetype
+    import pandas as pd
+
+    csms = pd.read_csv(SCOUT_CSMS_9)
+    csms_f = pd.read_csv(SCOUT_CSMS_F_9)
+    xls = pd.read_csv(SCOUT_XL_9)
+    err = pd.read_excel("data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx")
+
+    assert detect_scout_filetype(csms) == "scout_csms_unfiltered"
+    assert detect_scout_filetype(csms_f) == "scout_csms_filtered"
+    assert detect_scout_filetype(xls) == "scout_xl"
+
+    with pytest.raises(
+        ValueError,
+        match="Could not infer data source, are you sure you read a Scout result file?",
+    ):
+        _r = detect_scout_filetype(err)
