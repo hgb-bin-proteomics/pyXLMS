f96879eb0c65ddfeba7884ab8ccceeb75854e6d0
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-05-08 12:26:41 +0200

    add data files for test

---
+++ tests/test_parser_xldbse_scout.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# pyXLMS - TESTS
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+import pytest
+
+SCOUT_CSMS_9 = "data/scout/Cas9_Unfiltered_CSMs.csv"
+SCOUT_CSMS_F_9 = "data/scout/Cas9_Filtered_CSMs.csv"
+SCOUT_XL_9 = "data/scout/Cas9_Residue_Pairs.csv"
+SCOUT_CSMS_10 = "data/scout/Cas10_Unfiltered_CSMs.csv"
+SCOUT_CSMS_F_10 = "data/scout/Cas10_Filtered_CSMs.csv"
+SCOUT_XL_10 = "data/scout/Cas10_Residue_Pairs.csv"
+SCOUT_MM = "data/scout/Multi_Mod.csv"
