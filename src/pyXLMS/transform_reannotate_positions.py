1159df8ef5e66b5e5e9231348a0aa4d3ed5b89e9
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-05-22 17:15:04 +0200

    wip reannotate positions

--- src/pyXLMS/transform_reannotate_positions.py
+++ src/pyXLMS/transform_reannotate_positions.py
@@ -1,2 +1,47 @@
-def reannotate_positions():
-    return
+#!/usr/bin/env python3
+
+# 2024 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+from __future__ import annotations
+
+from Bio import SeqIO
+
+from .data import check_input
+from .data import create_parser_result
+from .transform_util import assert_data_type_same
+
+from typing import Optional
+from typing import Dict
+from typing import Tuple
+from typing import List
+from typing import Any
+
+
+def reannotate_positions(
+    data: List[Dict[str, Any]] | Dict[str, Any],
+    fasta: str
+) -> List[Dict[str, Any]] | Dict[str, Any]:
+    r"""
+    """
+    if type(data) == list:
+        _ok = assert_data_type_same(data)
+        reannoted = list()
+        if data[0]["data_type"] == "crosslink":
+            # todo
+            pass
+        elif data[0]["data_type"] == "crosslink-spectrum-match":
+            # todo
+            pass
+        else:
+            raise TypeError()
+        return reannoted
+    _ok = check_input(data, "data", dict)
+    if "data_type" not in data or data["data_type"] != "parser_result":
+        raise TypeError()
+    return create_parser_result(
+        search_engine = data["search_engine"],
+        csms = reannotate_positions(data["crosslink-spectrum-matches"], fasta) if data["crosslink-spectrum-matches"] is not None else None,
+        crosslinks= reannotate_positions(data["crosslinks"], fasta) if data["crosslinks"] is not None else None
+    )
