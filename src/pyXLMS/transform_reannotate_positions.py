9d2a3673f0ea7b9c96a928e07df672c28c863866
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-05-26 14:42:03 +0200

    add doc str for reannotate positions

--- src/pyXLMS/transform_reannotate_positions.py
+++ src/pyXLMS/transform_reannotate_positions.py
@@ -106,7 +106,48 @@ def reannotate_positions(
     fasta: str | BinaryIO,
     title_to_accession: Optional[Callable[[str], str]] = None
 ) -> List[Dict[str, Any]] | Dict[str, Any]:
-    r"""
+    r"""Reannotates protein crosslink positions for a given fasta file.
+
+    Reannotates the crosslink and peptide positions of the given cross-linked peptide pair and
+    the specified fasta file. Takes a list of crosslink-spectrum-matches or crosslinks, or a
+    parser_result as input.
+
+    Parameters
+    ----------
+    data : list of dict of str, any, or dict of str, any
+        A list of crosslink-spectrum-matches or crosslinks to annotate, or a parser_result.
+    fasta : str, or file stream
+        The name/path of the fasta file containing protein sequences or a file-like object/stream.
+    title_to_accession : callable, or None, default = None
+        A function that parses the protein accession from the fasta title/header. If None (default)
+        the function ``fasta_title_to_accession`` is used.
+
+    Returns
+    -------
+    list of dict of str, any, or dict of str, any
+        If a list of crosslink-spectrum-matches or crosslinks was provided, a list of annotated
+        crosslink-spectrum-matches or crosslinks is returned. If a parser_result was provided,
+        an annotated parser_result will be returned.
+
+    Raises
+    ------
+    TypeError
+        If a wrong data type is provided.
+
+    Examples
+    --------
+    >>> from pyXLMS.data import create_crosslink_min
+    >>> from pyXLMS.transform import reannotate_positions
+    >>> xls = [create_crosslink_min("ADANLDK", 7, "GNTDRHSIK", 9)]
+    >>> xls = reannotate_positions(xls, "data/_fasta/Cas9_plus10.fasta")
+    >>> xls[0]["alpha_proteins"]
+    ["Cas9"]
+    >>> xls[0]["alpha_proteins_crosslink_positions"]
+    [1293]
+    >>> xls[0]["beta_proteins"]
+    ["Cas9"]
+    >>> xls[0]["beta_proteins_crosslink_positions"]
+    [48]
     """
     if title_to_accession is not None:
         _ok = check_input(title_to_accession, "title_to_accession", Callable)
