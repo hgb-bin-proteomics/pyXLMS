0a6200805624d2245bb311ce3162e64225ff2f91
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-02 16:04:36 +0200

    finish docs for to proforma

--- src/pyXLMS/transform_to_proforma.py
+++ src/pyXLMS/transform_to_proforma.py
@@ -23,7 +23,7 @@ def __get_modified_peptide(
     crosslinker: Optional[str | float]
 ) -> str:
     r"""Returns the Proforma string for a single peptide.
-    
+
     Parameters
     ----------
     sequence : str
@@ -35,7 +35,7 @@ def __get_modified_peptide(
     crosslinker : str, or float, or None
         Optional name or mass of the crosslink reagent. If the name is given, it should be a valid
         name from XLMOD.
-        
+
     Returns
     -------
     str
@@ -43,6 +43,7 @@ def __get_modified_peptide(
 
     Notes
     -----
+    - This function should not be called directly, it is called from ``__to_proforma_csm()`` and ``__to_proforma_xl``.
     - Modifications with unknown mass are skipped.
     - If no modifications are given, only the crosslink modification will be encoded in the Proforma.
     - If no modifications are given and no crosslinker is given, the unmodified peptide Proforma will be returned.
@@ -70,6 +71,28 @@ def __get_modified_peptide(
 
 
 def __to_proforma_csm(csm: Dict[str, Any], crosslinker: Optional[str | float]) -> str:
+    r"""Returns the Proforma string for a single crosslink-spectrum-match.
+
+    Parameters
+    ----------
+    csm : dict of str, any
+        A pyXLMS crosslink-spectrum-match object. See ``data.create_csm()``.
+    crosslinker : str, or float, or None
+        Optional name or mass of the crosslink reagent. If the name is given, it should be a valid
+        name from XLMOD.
+
+    Returns
+    -------
+    str
+        The Proforma string of the crosslink-spectrum-match.
+
+    Notes
+    -----
+    - This function should not be called directly, it is called from ``to_proforma()``.
+    - Modifications with unknown mass are skipped.
+    - If no modifications are given, only the crosslink modification will be encoded in the Proforma.
+    - If no modifications are given and no crosslinker is given, the unmodified peptide Proforma will be returned.
+    """
     peptide_a = __get_modified_peptide(csm["alpha_peptide"], csm["alpha_modifications"], csm["alpha_peptide_crosslink_position"], crosslinker)
     peptide_b = __get_modified_peptide(csm["beta_peptide"], csm["beta_modifications"], csm["beta_peptide_crosslink_position"], crosslinker)
     if csm["charge"] is not None:
@@ -78,6 +101,28 @@ def __to_proforma_csm(csm: Dict[str, Any], crosslinker: Optional[str | float]) -
 
 
 def __to_proforma_xl(xl: Dict[str, Any], crosslinker: Optional[str | float]) -> str:
+    r"""Returns the Proforma string for a single crosslink.
+
+    Parameters
+    ----------
+    xl : dict of str, any
+        A pyXLMS crosslink object. See ``data.create_crosslink()``.
+    crosslinker : str, or float, or None
+        Optional name or mass of the crosslink reagent. If the name is given, it should be a valid
+        name from XLMOD.
+
+    Returns
+    -------
+    str
+        The Proforma string of the crosslink.
+
+    Notes
+    -----
+    - This function should not be called directly, it is called from ``to_proforma()``.
+    - Modifications with unknown mass are skipped.
+    - If no modifications are given, only the crosslink modification will be encoded in the Proforma.
+    - If no modifications are given and no crosslinker is given, the unmodified peptide Proforma will be returned.
+    """
     peptide_a = __get_modified_peptide(xl["alpha_peptide"], None, xl["alpha_peptide_crosslink_position"], crosslinker)
     peptide_b = __get_modified_peptide(xl["beta_peptide"], None, xl["beta_peptide_crosslink_position"], crosslinker)
     return f"{peptide_a}//{peptide_b}"
@@ -87,7 +132,86 @@ def to_proforma(
     data: Dict[str, Any] | List[Dict[str, Any]],
     crosslinker: Optional[str | float]
 ) -> str | List[str]:
-    r"""
+    r"""Returns the Proforma string for a single crosslink or crosslink-spectrum-match, or for
+    a list of crosslinks or crosslink-spectrum-matches.
+
+    Parameters
+    ----------
+    data : dict of str, any, or list of dict of str, any
+        A pyXLMS crosslink object, e.g. see ``data.create_crosslink()``. Or a pyXLMS crosslink-spectrum-match
+        object, e.g. see ``data.create_csm()``. Alternatively, a list of crosslinks or crosslink-spectrum-matches
+        can also be provided.
+    crosslinker : str, or float, or None
+        Optional name or mass of the crosslink reagent. If the name is given, it should be a valid
+        name from XLMOD. If the crosslink modification is contained in the crosslink-spectrum-match object
+        this parameter has no effect.
+
+    Returns
+    -------
+    str
+        The Proforma string of the crosslink or crosslink-spectrum-match. If a list was provided
+        a list containing all Proforma strings is returned.
+
+    Raises
+    ------
+    TypeError
+        If an unsupported data type is provided.
+
+    Notes
+    -----
+    - Modifications with unknown mass are skipped.
+    - If no modifications are given, only the crosslink modification will be encoded in the Proforma.
+    - If no modifications are given and no crosslinker is given, the unmodified peptide Proforma will be returned.
+
+    Examples
+    --------
+    >>> from pyXLMS.data import create_crosslink_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> xl = create_crosslink_min("PEPKTIDE", 4, "KPEPTIDE", 1)
+    >>> to_proforma(xl)
+    'KPEPTIDE//PEPKTIDE'
+
+    >>> from pyXLMS.data import create_crosslink_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> xl = create_crosslink_min("PEPKTIDE", 4, "KPEPTIDE", 1)
+    >>> to_proforma(xl, crosslinker="Xlink:DSSO")
+    'K[Xlink:DSSO]PEPTIDE//PEPK[Xlink:DSSO]TIDE'
+
+    >>> from pyXLMS.data import create_csm_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> csm = create_csm_min("PEPKTIDE", 4, "KPEPTIDE", 1, "RUN_1", 1)
+    >>> to_proforma(csm)
+    'KPEPTIDE//PEPKTIDE'
+
+    >>> from pyXLMS.data import create_csm_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> csm = create_csm_min("PEPKTIDE", 4, "KPEPTIDE", 1, "RUN_1", 1)
+    >>> to_proforma(csm, crosslinker="Xlink:DSSO")
+    'K[Xlink:DSSO]PEPTIDE//PEPK[Xlink:DSSO]TIDE'
+
+    >>> from pyXLMS.data import create_csm_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> csm = create_csm_min("PEPKTIDE", 4, "KPMEPTIDE", 1, "RUN_1", 1, modifications_b={3:("Oxidation", 15.994915)})
+    >>> to_proforma(csm, crosslinker="Xlink:DSSO")
+    'K[Xlink:DSSO]PM[+15.994915]EPTIDE//PEPK[Xlink:DSSO]TIDE'
+
+    >>> from pyXLMS.data import create_csm_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> csm = create_csm_min("PEPKTIDE", 4, "KPMEPTIDE", 1, "RUN_1", 1, modifications_b={3:("Oxidation", 15.994915)}, charge=3)
+    >>> to_proforma(csm, crosslinker="Xlink:DSSO")
+    'K[Xlink:DSSO]PM[+15.994915]EPTIDE//PEPK[Xlink:DSSO]TIDE/3'
+
+    >>> from pyXLMS.data import create_csm_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> csm = create_csm_min("PEPKTIDE", 4, "KPMEPTIDE", 1, "RUN_1", 1, modifications_a={4:("DSSO", 158.00376)}, modifications_b={1:("DSSO", 158.00376), 3:("Oxidation", 15.994915)}, charge=3)
+    >>> to_proforma(csm)
+    'K[+158.00376]PM[+15.994915]EPTIDE//PEPK[+158.00376]TIDE/3'
+
+    >>> from pyXLMS.data import create_csm_min
+    >>> from pyXLMS.transform import to_proforma
+    >>> csm = create_csm_min("PEPKTIDE", 4, "KPMEPTIDE", 1, "RUN_1", 1, modifications_a={4:("DSSO", 158.00376)}, modifications_b={1:("DSSO", 158.00376), 3:("Oxidation", 15.994915)}, charge=3)
+    >>> to_proforma(csm, crosslinker="Xlink:DSSO")
+    'K[+158.00376]PM[+15.994915]EPTIDE//PEPK[+158.00376]TIDE/3'
     """
     _ok = check_input_multi(crosslinker, "crosslinker", [str, float]) if crosslinker is not None else True
     if isinstance(data, list):
@@ -95,7 +219,9 @@ def to_proforma(
         proforma = list()
         for item in data:
             if "data_type" not in item or item["data_type"] not in ["crosslink", "crosslink-spectrum-match"]:
-                raise TypeError()
+                raise TypeError(
+                    "Unsupported data type for input data! Parameter data has to be a (list of) crosslink or crosslink-spectrum-match!"
+                    )
             if item["data_type"] == "crosslink":
                 proforma.append(__to_proforma_xl(item, crosslinker))
             else:
@@ -103,7 +229,9 @@ def to_proforma(
         return proforma
     _ok = check_input(data, "data", dict)
     if "data_type" not in data or data["data_type"] not in ["crosslink", "crosslink-spectrum-match"]:
-        raise TypeError()
+        raise TypeError(
+            "Unsupported data type for input data! Parameter data has to be a (list of) crosslink or crosslink-spectrum-match!"
+        )
     if data["data_type"] == "crosslink":
         return __to_proforma_xl(data, crosslinker)
     return __to_proforma_csm(data, crosslinker)
