9d48f94a74172ca66ebd746d735a8dee9a9b8185
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-02 17:57:41 +0200

    wip filter

---
+++ src/pyXLMS/transform_filter_protein.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+from __future__ import annotations
+
+import pandas as pd
+
+from .data import check_input
+from .data import check_input_multi
+
+from typing import Optional
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Any
