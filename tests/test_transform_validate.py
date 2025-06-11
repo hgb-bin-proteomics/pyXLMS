5f0f55fe6c6ff95df1740c4c34a39f65f4ee8327
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-11 17:47:25 +0200

    add more validation tests

--- tests/test_transform_validate.py
+++ tests/test_transform_validate.py
@@ -204,3 +204,275 @@ def test9():
     ):
         validated = validate(pr)
         assert validated["crosslink-spectrum-matches"] == []
+
+
+def test10():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    validated = validate(
+        pr["crosslink-spectrum-matches"], fdr=0.01, formula="(TD+DD)/TT"
+    )
+    assert get_fdr_strict(validated) < 0.01
+
+
+def test11():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    validated = validate(
+        pr["crosslink-spectrum-matches"], fdr=0.01, formula="(TD-DD)/TT"
+    )
+    assert get_fdr_relaxed(validated) < 0.01
+
+
+def test12():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    for csm in pr["crosslink-spectrum-matches"]:
+        csm["score"] = -csm["score"]
+    validated = validate(
+        pr["crosslink-spectrum-matches"],
+        fdr=0.01,
+        formula="(TD+DD)/TT",
+        score="lower_better",
+    )
+    assert get_fdr_strict(validated) < 0.01
+
+
+def test13():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    for csm in pr["crosslink-spectrum-matches"]:
+        csm["score"] = -csm["score"]
+    validated = validate(
+        pr["crosslink-spectrum-matches"],
+        fdr=0.01,
+        formula="(TD-DD)/TT",
+        score="lower_better",
+    )
+    assert get_fdr_relaxed(validated) < 0.01
+
+
+def test14():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+    from pyXLMS.transform import filter_crosslink_type
+
+    pr = read(
+        "data/_test/validate/csms.xlsx",
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    for csm in pr["crosslink-spectrum-matches"]:
+        csm["score"] = -csm["score"]
+    validated = validate(
+        pr["crosslink-spectrum-matches"],
+        fdr=0.01,
+        formula="(TD+DD)/TT",
+        score="lower_better",
+        separate_intra_inter=True,
+    )
+    separate = filter_crosslink_type(validated)
+    assert get_fdr_strict(separate["Intra"]) < 0.01
+    assert get_fdr_strict(separate["Inter"]) < 0.01
+
+
+def test15():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+    from pyXLMS.transform import filter_crosslink_type
+
+    pr = read(
+        "data/_test/validate/csms.xlsx",
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    for csm in pr["crosslink-spectrum-matches"]:
+        csm["score"] = -csm["score"]
+    validated = validate(
+        pr["crosslink-spectrum-matches"],
+        fdr=0.01,
+        formula="(TD-DD)/TT",
+        score="lower_better",
+        separate_intra_inter=True,
+    )
+    separate = filter_crosslink_type(validated)
+    assert get_fdr_relaxed(separate["Intra"]) < 0.01
+    assert get_fdr_relaxed(separate["Inter"]) < 0.01
+
+
+def test16():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    for i, csm in enumerate(pr["crosslink-spectrum-matches"]):
+        csm["score"] = -csm["score"]
+        if i < 5:
+            csm["alpha_decoy"] = None
+        elif i < 10:
+            csm["beta_decoy"] = None
+    validated = validate(
+        pr["crosslink-spectrum-matches"],
+        fdr=0.01,
+        formula="(TD+DD)/TT",
+        score="lower_better",
+        ignore_missing_labels=True,
+    )
+    assert get_fdr_strict(validated) < 0.01
+
+
+def test17():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    for i, csm in enumerate(pr["crosslink-spectrum-matches"]):
+        csm["score"] = -csm["score"]
+        if i < 5:
+            csm["alpha_decoy"] = None
+        elif i < 10:
+            csm["beta_decoy"] = None
+    validated = validate(
+        pr["crosslink-spectrum-matches"],
+        fdr=0.01,
+        formula="(TD-DD)/TT",
+        score="lower_better",
+        ignore_missing_labels=True,
+    )
+    assert get_fdr_relaxed(validated) < 0.01
+
+
+def test18():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    for i, csm in enumerate(pr["crosslink-spectrum-matches"]):
+        csm["score"] = -csm["score"]
+        if i < 5:
+            csm["alpha_decoy"] = None
+        elif i < 10:
+            csm["beta_decoy"] = None
+    err_str = (
+        r"Can't validate data if 'score' or target\/decoy labels are missing! Selecting 'ignore_missing_labels \= True' will ignore crosslinks and crosslink-spectrum-matches "
+        r"that don't have a valid target\/decoy label and filter them out!"
+    )
+    with pytest.raises(
+        ValueError,
+        match=err_str,
+    ):
+        _validated = validate(
+            pr["crosslink-spectrum-matches"],
+            fdr=0.01,
+            formula="(TD+DD)/TT",
+            score="lower_better",
+            ignore_missing_labels=False,
+        )
+
+
+def test19():
+    from pyXLMS.parser import read
+    from pyXLMS.transform import validate
+
+    pr = read(
+        [
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+            "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_Crosslinks.xlsx",
+        ],
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    assert len(pr["crosslink-spectrum-matches"]) == 826
+    assert len(pr["crosslinks"]) == 300
+    for i, csm in enumerate(pr["crosslink-spectrum-matches"]):
+        csm["score"] = -csm["score"]
+        if i < 5:
+            csm["alpha_decoy"] = None
+        elif i < 10:
+            csm["beta_decoy"] = None
+    err_str = (
+        r"Can't validate data if 'score' or target\/decoy labels are missing! Selecting 'ignore_missing_labels \= True' will ignore crosslinks and crosslink-spectrum-matches "
+        r"that don't have a valid target\/decoy label and filter them out!"
+    )
+    with pytest.raises(
+        ValueError,
+        match=err_str,
+    ):
+        _validated = validate(
+            pr["crosslink-spectrum-matches"],
+            fdr=0.01,
+            formula="(TD-DD)/TT",
+            score="lower_better",
+            ignore_missing_labels=False,
+        )
