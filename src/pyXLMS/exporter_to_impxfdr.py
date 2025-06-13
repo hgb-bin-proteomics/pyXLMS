ac69dfdfb8af711c43e8eecd3692d0916704d646
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-13 18:19:53 +0200

    wip to imp x fdr

---
+++ src/pyXLMS/exporter_to_impxfdr.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+from __future__ import annotations
+
+from .exporter_to_msannika import to_msannika
+
+from typing import BinaryIO
+from typing import Dict
+from typing import Any
+from typing import Tuple
+from typing import List
+
+
+def to_impxfdr(
+    data: List[Dict[str, Any]],
+    filename: str,
+) -> pd.DataFrame:
+    # assert csms
+    return to_msannika(data, filename, format="excel")
