a05f5555b27581cbf71f55d127fb58271e5deee9
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-16 17:02:35 +0200

    finalize impl imp x fdr exporter

--- src/pyXLMS/exporter_to_impxfdr.py
+++ src/pyXLMS/exporter_to_impxfdr.py
@@ -8,6 +8,8 @@ from __future__ import annotations
 
 import pandas as pd
 
+from .data import check_input
+from .transform_filter import filter_target_decoy
 from .exporter_to_msannika import to_msannika
 
 from typing import Optional
@@ -19,6 +21,9 @@ from typing import List
 def to_impxfdr(
     data: List[Dict[str, Any]],
     filename: Optional[str],
+    targets_only: bool = True,
 ) -> pd.DataFrame:
-    # assert csms
+    _ok = check_input(targets_only, "targets_only", bool)
+    if targets_only:
+        data = filter_target_decoy(data)["Target-Target"]
     return to_msannika(data, filename, format="xlsx")
