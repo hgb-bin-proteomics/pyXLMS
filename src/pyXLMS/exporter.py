5c0cd1a186238cde4069e69215139ceb97f86b17
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-18 18:52:38 +0200

    import xlindb exporter to namespace

--- src/pyXLMS/exporter.py
+++ src/pyXLMS/exporter.py
@@ -5,6 +5,7 @@
 # micha.birklbauer@gmail.com
 
 from .exporter_to_xmas import to_xmas  # noqa: F401
+from .exporter_to_xlinkdb import to_xlinkdb  # noqa: F401
 from .exporter_to_impxfdr import to_impxfdr  # noqa: F401
 from .exporter_to_msannika import to_msannika  # noqa: F401
 from .exporter_to_msannika import get_msannika_crosslink_sequence  # noqa: F401
