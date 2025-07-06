be19a37953800a387e6d6a3e75a6efdca987949a
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-06 02:24:12 +0200

    add gui test

---
+++ gui/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# pyXLMS GUI - TEST
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+
+def test1():
+    from streamlit.testing.v1 import AppTest
+
+    at = AppTest.from_file("streamlit_app.py")
+    at.run()
+    assert not at.exception
