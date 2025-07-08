74f7b5ead73244dc0a1c12df24247b28476efa7a
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-08 16:00:27 +0200

    start with plotting submodule

---
+++ src/pyXLMS/plotting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+from .plotting_score_distribution import plot_score_distribution  # noqa: F401
