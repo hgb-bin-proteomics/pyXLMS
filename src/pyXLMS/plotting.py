efc54e858110bee4c780f1337f34daf4d8fb519d
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-09 15:42:31 +0200

    add plot venn diagram

--- src/pyXLMS/plotting.py
+++ src/pyXLMS/plotting.py
@@ -4,4 +4,6 @@
 # https://github.com/michabirklbauer/
 # micha.birklbauer@gmail.com
 
+from .plotting_plot_venn_diagram import venn  # noqa: F401
+from .plotting_plot_venn_diagram import plot_venn_diagram  # noqa: F401
 from .plotting_plot_score_distribution import plot_score_distribution  # noqa: F401
