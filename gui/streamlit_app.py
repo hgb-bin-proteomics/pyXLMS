ef512c31a3ea27783b2c9f645c55b6e1a69bd355
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-10 11:10:38 +0200

    add crosslinks imp-x-fdr exporter

--- gui/streamlit_app.py
+++ gui/streamlit_app.py
@@ -1046,6 +1046,66 @@ def export_tab():
                 "xlms-tools",
                 "XMAS",
             ]
+            export_crosslinks_picker = st.selectbox(
+                "Export crosslinks to:",
+                options=export_crosslinks_options,
+                index=None,
+                help="Chose a format to export the crosslinks to.",
+            )
+            if export_crosslinks_picker is None:
+                pass
+            elif export_crosslinks_picker == "IMP-X-FDR":
+                export_crosslinks_impxfdr_info = st.info(
+                    "To export to IMP-X-FDR your crosslinks should be **unique** and **not** "
+                    + "**contain any decoy matches**! Usually you would also want to filter for high-confidence matches "
+                    + "to compare FDR estimation to the experimentally validated FDR! You can check this in the "
+                    + "**'Load Data'** tab in the **'Summary Statistics'** of your loaded result!"
+                )
+                export_crosslinks_impxfdr_button = st.button(
+                    "Export to IMP-X-FDR!",
+                    type="primary",
+                    use_container_width=True,
+                    key="export_crosslinks_impxfdr_button",
+                )
+                if export_crosslinks_impxfdr_button:
+                    with st.spinner(
+                        "Exporting crosslinks to IMP-X-FDR...",
+                        show_time=True,
+                    ):
+                        try:
+                            st.session_state["export_crosslinks_impxfdr"] = (
+                                exporter.to_impxfdr(crosslinks, filename=None)
+                            )
+                        except Exception as e:
+                            _ = st.error(
+                                "Something went wrong! This is most likely due to missing information in the results!",
+                                icon="⚠️",
+                            )
+                            with st.expander("Show exception"):
+                                _ = st.exception(e)
+                if (
+                    "export_crosslinks_impxfdr" in st.session_state
+                    and st.session_state["export_crosslinks_impxfdr"] is not None
+                ):
+                    export_crosslinks_impxfdr_download_info = st.markdown(
+                        "Your exported crosslinks in IMP-X-FDR format are ready for download:"
+                    )
+                    export_crosslinks_impxfdr_download = st.download_button(
+                        label="Download in IMP-X-FDR format!",
+                        data=dataframe_to_xlsx_stream(
+                            st.session_state["export_crosslinks_impxfdr"],
+                            sheet_name="imp-x-fdr",
+                            index=False,
+                        ),
+                        file_name="crosslinks_imp-x-fdr.xlsx",
+                        on_click="ignore",
+                        type="primary",
+                        mime="application/vnd.ms-excel",
+                        icon=":material/download:",
+                        use_container_width=True,
+                        help="Downloads the exported crosslinks in IMP-X-FDR format.",
+                        key="export_crosslinks_impxfdr_download",
+                    )
     if "aggregated" in st.session_state and st.session_state["aggregated"] is not None:
         aggregated_crosslinks = st.session_state["aggregated"]
         export_aggregated_crosslinks_header = st.subheader(
