605be55d81e74462a6a0bc7bc8d149308cfc24be
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-23 14:44:09 +0200

    import plot protein distribution

--- src/pyXLMS/plotting/__init__.py
+++ src/pyXLMS/plotting/__init__.py
@@ -9,9 +9,11 @@ __all__ = [
     "plot_venn_diagram",
     "plot_score_distribution",
     "plot_target_decoy_distribution",
+    "plot_protein_distribution",
 ]
 
 from .plot_venn_diagram import venn
 from .plot_venn_diagram import plot_venn_diagram
 from .plot_score_distribution import plot_score_distribution
 from .plot_target_decoy_distribution import plot_target_decoy_distribution
+from .plot_protein_distribution import plot_protein_distribution
