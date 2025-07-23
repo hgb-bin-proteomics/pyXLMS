c2a8611e8ebedb2d3d312a943f266a67a11f60d3
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-23 16:28:40 +0200

    impl peptide pair distribution plot

--- src/pyXLMS/plotting/__init__.py
+++ src/pyXLMS/plotting/__init__.py
@@ -10,6 +10,7 @@ __all__ = [
     "plot_score_distribution",
     "plot_target_decoy_distribution",
     "plot_protein_distribution",
+    "plot_peptide_pair_distribution",
 ]
 
 from .plot_venn_diagram import venn
@@ -17,3 +18,4 @@ from .plot_venn_diagram import plot_venn_diagram
 from .plot_score_distribution import plot_score_distribution
 from .plot_target_decoy_distribution import plot_target_decoy_distribution
 from .plot_protein_distribution import plot_protein_distribution
+from .plot_peptide_pair_distribution import plot_peptide_pair_distribution
