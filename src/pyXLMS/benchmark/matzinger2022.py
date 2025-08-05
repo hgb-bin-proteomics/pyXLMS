b9bc72733a68f04b53edb4f3e86328f5e3af1100
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-08-05 11:21:05 +0200

    add matzinger 2022 template

---
+++ src/pyXLMS/benchmark/matzinger2022.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+PRIDE_URL = "https://www.ebi.ac.uk/pride/archive/projects/PXD029252"
+PROTEOMEXCHANGE_URL = (
+    "https://proteomecentral.proteomexchange.org/cgi/GetDataset?ID=PXD029252"
+)
+PUBLICATION_URL = "https://doi.org/10.1038/s41467-022-31701-w"
+DATASETS = []
+LIBRARIES = {}
