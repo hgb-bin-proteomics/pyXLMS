695fd4723ee6d300f5f98ec23823a6b667154fa6
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-02-13 16:19:02 +0100

    ignore argument type

--- src/pyXLMS/exporter/to_pyxlinkviewer.py
+++ src/pyXLMS/exporter/to_pyxlinkviewer.py
@@ -241,7 +241,7 @@ def __get_xl_position_and_chain_in_protein(
                                     seqA.replace("-", ""), pdb_sequence
                                 )
                             ],
-                            a_start,
+                            a_start,  # pyright: ignore[reportArgumentType]
                         )
                         xl_position = pep_pos_in_protein + xl_pos_in_alignment
                         xl_chain = pdb_chains[xl_position]
@@ -255,7 +255,7 @@ def __get_xl_position_and_chain_in_protein(
                             m.start()
                             for m in re.finditer(seqA.replace("-", ""), pdb_sequence)
                         ],
-                        a_start,
+                        a_start,  # pyright: ignore[reportArgumentType]
                     )
                     xl_position = pep_pos_in_protein + xl_pos_in_alignment
                     xl_chain = pdb_chains[xl_position]
