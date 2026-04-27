07450a1ce2830381c4d782566da569147749b1bb
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-04-27 21:42:33 +0200

    update docstring

--- src/pyXLMS/transform/annotate_string_scores.py
+++ src/pyXLMS/transform/annotate_string_scores.py
@@ -256,16 +256,19 @@ def annotate_string_scores(
     KeyError
         If the organism could not be resolved to a taxon identifier.
     RuntimeError
+        If data with more than 2000 proteins/STRING IDs is provided.
+    RuntimeError
         If ``verbose = 2`` and the API request failed.
 
     Warns
     -----
+    RuntimeWarning
         If ``verbose = 1`` and the API request failed.
 
     Notes
     -----
-    STRING limits the number of nodes via the API two 2000 - exceeding that limit will return an empty network
-    or raise an error (if ``verbose = 2``).
+    STRING limits the number of nodes via the API two 2000 - exceeding that limit will return an empty
+    network/no annotation or raise an error (if ``verbose = 2``).
 
     Examples
     --------
