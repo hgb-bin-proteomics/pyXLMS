b1a81547729ae6b86019663d0dbd50eb2c06793c
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-05-13 18:52:40 +0200

    add xiview csm tests

--- tests/test_exporter_to_xiview.py
+++ tests/test_exporter_to_xiview.py
@@ -178,7 +178,7 @@ def test8():
 
     with pytest.raises(
         ValueError,
-        match="Provided crosslinks contain no elements!",
+        match="Provided data contains no elements!",
     ):
         _df = to_xiview([], filename="crosslinks_xiVIEW.csv")
 
@@ -196,3 +196,70 @@ def test9():
         match="Can't export to xiVIEW because not all necessary information is available!",
     ):
         _df = to_xiview(crosslinks, filename=None)
+
+
+def test10():
+    import os
+    from pyXLMS.exporter import to_xiview
+    from pyXLMS.parser import read
+    from pyXLMS.transform import targets_only
+    from pyXLMS.transform import filter_proteins
+
+    pr = read(
+        "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    csms = targets_only(pr)["crosslink-spectrum-matches"]
+    cas9 = filter_proteins(csms, proteins=["Cas9"])["Both"]
+    for csm in cas9:
+        csm["score"] = None
+    df = to_xiview(cas9, filename="xiview_csms.csv")
+    assert os.path.isfile("xiview_csms.csv")
+    assert df.shape[0] == len(cas9)
+    assert df.shape[1] == 13
+    cols = df.columns.values.tolist()
+    for col in XINET_COLS:
+        assert col in cols
+    assert "ScanId" in cols
+    assert "PeakListFileName" in cols
+    assert "Score" not in cols
+
+
+def test11():
+    from pyXLMS.exporter import to_xiview
+    from pyXLMS.parser import read
+    from pyXLMS.transform import targets_only
+    from pyXLMS.transform import filter_proteins
+
+    pr = read(
+        "data/ms_annika/XLpeplib_Beveridge_QEx-HFX_DSS_R1_CSMs.xlsx",
+        engine="MS Annika",
+        crosslinker="DSS",
+    )
+    csms = targets_only(pr)["crosslink-spectrum-matches"]
+    cas9 = filter_proteins(csms, proteins=["Cas9"])["Both"]
+    df = to_xiview(cas9, filename=None)
+    assert df.shape[0] == len(cas9)
+    assert df.shape[1] == 14
+    cols = df.columns.values.tolist()
+    for col in XINET_COLS:
+        assert col in cols
+    assert "ScanId" in cols
+    assert "PeakListFileName" in cols
+    assert "Score" in cols
+    assert "Decoy 1" in cols
+    assert "Decoy 2" in cols
+    for i, row in df.iterrows():
+        assert str(row["PepSeq1"]) == cas9[i]["alpha_peptide"]
+        assert str(row["PepSeq2"]) == cas9[i]["beta_peptide"]
+        assert int(row["PepPos1"]) == cas9[i]["alpha_proteins_peptide_positions"][0]
+        assert int(row["PepPos2"]) == cas9[i]["beta_proteins_peptide_positions"][0]
+        assert int(row["LinkPos1"]) == cas9[i]["alpha_peptide_crosslink_position"]
+        assert int(row["LinkPos2"]) == cas9[i]["beta_peptide_crosslink_position"]
+        assert str(row["Protein1"]) == cas9[i]["alpha_proteins"][0]
+        assert str(row["Protein2"]) == cas9[i]["beta_proteins"][0]
+        assert int(row["ScanId"]) == cas9[i]["scan_nr"]
+        assert str(row["PeakListFileName"]) == cas9[i]["spectrum_file"]
+        assert str(row["Decoy 1"]) == "FALSE"
+        assert str(row["Decoy 2"]) == "FALSE"
