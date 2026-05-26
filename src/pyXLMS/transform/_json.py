dc15a706829425cfb736067a9f0bfa73539a8487
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-05-26 13:04:49 +0200

    impl from json

--- src/pyXLMS/transform/_json.py
+++ src/pyXLMS/transform/_json.py
@@ -8,6 +8,9 @@ from __future__ import annotations
 
 import os
 import json
+
+from pydantic import ValidationError
+
 from ..data._csm import CrosslinkSpectrumMatch
 from ..data._crosslink import Crosslink
 from ..data._parser_result import ParserResult
@@ -46,10 +49,33 @@ def to_json(
     return json.dumps(json_data, ensure_ascii=ensure_ascii, indent=indent)
 
 
-def from_json(json_input: str | BinaryIO) -> None:
+def from_json(
+    json_input: str | BinaryIO,
+) -> List[CrosslinkSpectrumMatch] | List[Crosslink] | ParserResult:
+    json_data: List[Dict[str, Any]] | Dict[str, Any] | None = None
     if isinstance(json_input, str):
         if os.path.isfile(json_input):
-            return
+            with open(json_input, "r", encoding="utf-8") as f:
+                json_data = json.load(f)
         else:
-            return
-    return
+            json_data = json.loads(json_input)
+    else:
+        json_input.seek(0)
+        json_data = json.load(json_input)
+    if json_data is None:
+        raise ValueError()
+    if isinstance(json_data, list):
+        maybe_csms_or_xls = list()
+        for item in json_data:
+            maybe_csm_or_xl: CrosslinkSpectrumMatch | Crosslink | None = None
+            try:
+                maybe_csm_or_xl = CrosslinkSpectrumMatch.model_validate(
+                    item, strict=False
+                )
+            except ValidationError as _e:
+                maybe_csm_or_xl = Crosslink.model_validate(item, strict=False)
+            if maybe_csm_or_xl is None:
+                raise ValueError()
+            maybe_csms_or_xls.append(maybe_csm_or_xl)
+        return assert_csms_or_xls(maybe_csms_or_xls)
+    return ParserResult.model_validate(json_data, strict=False)
