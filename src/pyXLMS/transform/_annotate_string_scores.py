c36e82298e6a7d8004611eda9b0587e85ff5a7aa
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-05-27 17:13:32 +0200

    fix assignment

--- src/pyXLMS/transform/_annotate_string_scores.py
+++ src/pyXLMS/transform/_annotate_string_scores.py
@@ -510,7 +510,7 @@ def annotate_string_scores(
                                     if network[key]["score"] is not None:
                                         string_scores.append(network[key]["score"])  # pyright: ignore[reportArgumentType] # ty: ignore[invalid-argument-type]
                 if item["additional_information"] is None:
-                    item["additional_information"] = dict()
+                    item.additional_information = dict()
                 item["additional_information"][
                     "pyXLMS_annotated_STRING_interactions"
                 ] = string_items
